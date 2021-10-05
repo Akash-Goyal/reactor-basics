a8328a94a97abdcc0b796b7c8d85615587322160
Akash Goyal <Akash.Goyal1@target.com>
2021-10-05 08:23:35 +0530

    Initial Commit

---
+++ src/main/java/org/example/L01_Mono.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class L01_Mono {
+
+    public static void main(String[] args) {
+
+//        Mono.from
+
+    }
+
+}
---
+++ src/test/java/example/L01_Mono.java
@@ -0,0 +1,13 @@
+package example;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class L01_Mono {
+
+    @Test
+    public void test(){
+        log.info("Finally");
+    }
+}
